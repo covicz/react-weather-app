6cdf95c96071a4a0c1fdbff5f1133f6fe8cf4994
Paulina <konieczkowiczp@gmail.com>
2023-01-26 15:01:58 +0100

    Added the github url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App 
+      <h1>Weather App </h1>
+      <footer>
+        Coded by Paulina Konieczkowicz and is open-sourced on{" "}
+        <a
+          href="https://github.com/covicz/react-weather-app.git"
+          terget="_blank">
+            GitHub
+        </a>
+      </footer>
     </div>
   );
 }
