011e34c57738790d7e645a0a134ae0e919fb6971
Paulina <konieczkowiczp@gmail.com>
2023-01-26 15:20:01 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App </h1>
       <footer>
         Coded by Paulina Konieczkowicz and is open-sourced on{" "}
@@ -13,6 +14,7 @@ export default function App() {
             GitHub
         </a>
       </footer>
+      </div>
     </div>
   );
 }
